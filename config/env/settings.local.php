9e3cc0e645eb997106284f15df16dc929e963270
Luiz Alberto <madeinnordeste@gmail.com>
2023-08-13 08:45:38 -0300

    start

---
+++ config/env/settings.local.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal9',
+  'username' => 'drupal9',
+  'password' => 'drupal9',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+
+
